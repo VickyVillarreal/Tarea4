c137c07bdb19cc6823ad5b8d28257b7c333c7264
Ana Victoria Villarreal <vickypop95@gmail.com>
2018-03-12 19:25:30 -0600

    git remote add origin https://github.com/VickyVillarreal/Tarea5.git

---
+++ app/src/main/java/com/example/jorgeurrea/tarea4/ActivitySlashScreen.java
@@ -0,0 +1,13 @@
+package com.example.jorgeurrea.tarea4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySlashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slash_screen);
+    }
+}
---
+++ app/src/main/java/com/example/jorgeurrea/tarea4/Beans/WebAppInterface.java
@@ -0,0 +1,8 @@
+package com.example.jorgeurrea.tarea4.Beans;
+
+/**
+ * Created by Jorge Urrea on 12/03/2018.
+ */
+
+public class WebAppInterface {
+}
